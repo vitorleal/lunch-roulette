704a6820314ec4f65b1e05330171bab5bdb239e7
Vitor Leal <vitorleal1@gmail.com>
2013-08-30 18:17:18 -0300

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+(function() {
+
+  var lunchRoulette = angular.module('lunchRoulette', []).
+    config(function($routeProvider, $locationProvider) {
+      $routeProvider.
+        when('/', {
+          controller : FriendList,
+          templateUrl: 'public/templates/friendsList.html'
+        }).
+        otherwise({ redirectTo: '/' });
+
+      $locationProvider.html5Mode(true);
+    });
+
+  var FriendList = function($scope) {
+    $scope.friends = [
+      { name: 'David',   invited: false, thumb: 'http://placehold.it/40x40' },
+      { name: 'Erico',   invited: false, thumb: 'http://placehold.it/40x40' },
+      { name: 'Sheldon', invited: false, thumb: 'http://placehold.it/40x40' },
+      { name: 'João',    invited: false, thumb: 'http://placehold.it/40x40' },
+      { name: 'Fábio',   invited: false, thumb: 'http://placehold.it/40x40' },
+      { name: 'Marcos',  invited: false, thumb: 'http://placehold.it/40x40' }
+    ];
+  };
+
+})();
+
